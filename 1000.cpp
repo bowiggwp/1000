e30668ba3e3f14a870ac7de20b3dd53644c16e71
bowiggwp <118131019+bowiggwp@users.noreply.github.com>
2022-11-13 22:13:50 +0300

    Add files via upload

---
+++ 1000.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int i;
+	int s = 0;
+
+	for (i = 1; i <= 1000; i++)
+	{
+		s = s + i;
+	}
+	cout << "сумма числе от 1 до 1000 = " << s << endl;
+	return 0;
+}
